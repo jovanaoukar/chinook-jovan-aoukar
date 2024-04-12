4dda3384f54f089c05fb1576a6f59523058893ea
Jovan Aoukar <aoukarjovan@hotmail.fr>
2024-04-12 17:06:02 +0200

    Add list of tracks page redirected for each album from the index.html page

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Album
 
@@ -6,3 +6,8 @@ def index(request):
     albums_list = Album.objects.order_by("title")
     context = {"albums_list": albums_list}
     return render(request, "disks/index.html", context)
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    context = {"album": album}
+    return render(request, "disks/detail.html", context)
