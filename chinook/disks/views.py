ec20d81c74accb8835f9a68e3d4dd8c57e9e77b5
Jovan Aoukar <aoukarjovan@hotmail.fr>
2024-04-12 15:38:12 +0200

    Add index.html in disks app to show the list of albums

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Album
+
+def index(request):
+    albums_list = Album.objects.order_by("title")
+    context = {"albums_list": albums_list}
+    return render(request, "disks/index.html", context)
