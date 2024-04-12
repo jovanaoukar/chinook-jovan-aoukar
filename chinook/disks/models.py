1c90986a1777e53bae4ba36520be5a11c040051c
Jovan Aoukar <aoukarjovan@hotmail.fr>
2024-04-12 15:15:28 +0200

    Add basic version of Artist, Album and Track models

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200, null=True)
+    milliseconds = models.IntegerField(default=0)
+    bytes = models.IntegerField(default=0)
+    unitPrice = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
